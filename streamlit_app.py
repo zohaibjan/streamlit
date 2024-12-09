dc5d198a022c7dd537665b589e41e7eac6da3b4d
Muhammad Zohaib Jan <zohaibjan@gmail.com>
2024-12-09 15:05:16 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,24 @@ st.title("🤖 Machine Learning App")
 
 st.write("Welcome to world of Machine Learning with Streamlit.")
 
-# read the csv file
-df = pd.read_csv("data/penguins_cleaned.csv")
-df
+with st.expander("Data"):
+    st.write("**Raw Data**")
+    # read the csv file
+    df = pd.read_csv("data/penguins_cleaned.csv")
+    df
+    # define and display
+    st.write("**X**")
+    X = df.drop("species", axis=1)
+    X
+
+    st.write("**y**")
+    y = df.species
+    y
+
+with st.expander("Data Visualization"):
+    st.scatter_chart(
+        df,
+        x="bill_length_mm",
+        y="body_mass_g",
+        color="species",
+    )
