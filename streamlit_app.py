08125267a93f00e969d9220faf03c1ee65c36a39
Muhammad Zohaib Jan <zohaibjan@gmail.com>
2024-12-09 15:04:50 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+# import pandas to read the our data file
+import pandas as pd
+
 st.title("🤖 Machine Learning App")
 
 st.write("Welcome to world of Machine Learning with Streamlit.")
+
+# read the csv file
+df = pd.read_csv("data/penguins_cleaned.csv")
+df
