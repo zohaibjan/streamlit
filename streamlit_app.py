a2f431e6f08e0eb72372355c4f5efa85f07f14fb
Muhammad Zohaib Jan <zohaibjan@gmail.com>
2024-12-09 15:06:19 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,87 @@ with st.expander("Data Visualization"):
         y="body_mass_g",
         color="species",
     )
+
+# Interactivity
+# Columns:
+# 'species', 'island', 'bill_length_mm', 'bill_depth_mm',
+# 'flipper_length_mm', 'body_mass_g', 'sex'
+with st.sidebar:
+    st.header("Input Features")
+    # Islands
+    islands = df.island.unique().astype(str)
+    island = st.selectbox(
+        "Island",
+        islands,
+    )
+    # Bill Length
+    min, max, mean = (
+        df.bill_length_mm.min(),
+        df.bill_length_mm.max(),
+        df.bill_length_mm.mean().round(2),
+    )
+    bill_length_mm = st.slider(
+        "Bill Length(mm)",
+        min_value=min,
+        max_value=max,
+        value=mean,
+    )
+    # Bill Depth
+    min, max, mean = (
+        df.bill_depth_mm.min(),
+        df.bill_depth_mm.max(),
+        df.bill_depth_mm.mean().round(2),
+    )
+    bill_depth_mm = st.slider(
+        "Bill Depth(mm)",
+        min_value=min,
+        max_value=max,
+        value=mean,
+    )
+    # Filpper Length
+    min, max, mean = (
+        df.flipper_length_mm.min().astype(float),
+        df.flipper_length_mm.max().astype(float),
+        df.flipper_length_mm.mean().round(2),
+    )
+    flipper_length_mm = st.slider(
+        "Flipper Length(mm)",
+        min_value=min,
+        max_value=max,
+        value=mean,
+    )
+    # Body Mass
+    min, max, mean = (
+        df.body_mass_g.min().astype(float),
+        df.body_mass_g.max().astype(float),
+        df.body_mass_g.mean().round(2),
+    )
+    body_mass_g = st.slider(
+        "Body Mass(g)",
+        min_value=min,
+        max_value=max,
+        value=mean,
+    )
+    # Gender
+    gender = st.radio(
+        "Gender",
+        ("male", "female"),
+    )
+
+# Dataframes for Input features
+data = {
+    "island": island,
+    "bill_length_mm": bill_length_mm,
+    "bill_depth_mm": bill_depth_mm,
+    "flipper_length_mm": flipper_length_mm,
+    "body_mass_g": body_mass_g,
+    "sex": gender,
+}
+input_df = pd.DataFrame(data, index=[0])
+input_penguins = pd.concat([input_df, X], axis=0)
+
+with st.expander("Input Features"):
+    st.write("**Input Penguins**")
+    input_df
+    st.write("**Combined Penguins Data**")
+    input_penguins
